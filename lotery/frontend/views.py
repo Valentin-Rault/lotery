9b33423ac041285a76306a6d87f2e62481a84067
Valentin Rault <rault.valentin@gmail.com>
2021-07-17 15:00:29 +0200

    Create view to retrieve random words
    Create async function to fetch a random word from db
    Create sync function to retrieve data from async function
    Display data in prize.html

--- lotery/frontend/views.py
+++ lotery/frontend/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
+from asgiref.sync import async_to_sync
 import aiohttp
 import asyncio
 
+
 # Create your views here.
-
-from aiohttp import web
-
-
 @asyncio.coroutine
-def getRandomPrize(request):
-    return web.Response(text='Hello World')
+async def getPrize(url, f=lambda x: x):
+    async with aiohttp.ClientSession() as client_session:
+        async with client_session.get(url) as client_response:
+            data = await client_response.json()
+            print(await client_response.text())
+        return f(data)
+
+def showRandomPrize(request):
+    def make_upper(dic):
+        dic['word'] = dic['word'].upper()
+        return dic
+    data = async_to_sync(getPrize)('http://localhost:8000/api/random-word', make_upper)
+    return render(request, 'prize.html', data)
