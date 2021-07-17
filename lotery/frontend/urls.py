3e9e5f35d2dc1a6fe5887a427a97b6a8b96c32ad
Valentin Rault <rault.valentin@gmail.com>
2021-07-17 11:52:09 +0200

    Create view and url for getRandomPrize

--- lotery/frontend/urls.py
+++ lotery/frontend/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from django.views.generic.base import TemplateView
 
+from .views import getRandomPrize
+
 urlpatterns = [
     path('', TemplateView.as_view(template_name='base.html'), name='home'),
-    path('getPrize', TemplateView.as_view(template_name='prize.html'), name='prize'),
+    path('getPrize', getRandomPrize(), name='prize'),
 ]
--- lotery/frontend/views.py
+++ lotery/frontend/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+import aiohttp
+import asyncio
 
 # Create your views here.
+
+from aiohttp import web
+
+
+@asyncio.coroutine
+def getRandomPrize(request):
+    return web.Response(text='Hello World')
