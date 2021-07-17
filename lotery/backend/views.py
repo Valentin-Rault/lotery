d0f4152ff53154cef31e4a398375f614163a45a5
Valentin Rault <rault.valentin@gmail.com>
2021-07-17 10:04:29 +0200

    Creation of Random word view
    Addition of gitignore

--- lotery/backend/views.py
+++ lotery/backend/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from random import randint
+
+from .models import Words
+from .serializers import WordsSerializer
 
 # Create your views here.
+class RandomWord(APIView):
+    def get(self, request, format=None):
+        length = Words.objects.count()
+        if length:
+            random_word = Words.objects.all()[randint(0, length - 1)]
+            word = WordsSerializer(random_word).data
+            return Response(word, status=status.HTTP_200_OK)
+        return Response({'message': 'No word found'}, status=status.HTTP_404_NOT_FOUND)
